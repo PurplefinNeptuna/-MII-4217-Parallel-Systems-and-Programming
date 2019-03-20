9c4479c5ce5767f2e87a876bf10a2490b35a79db
PurplefinNeptuna <ilhamaryasuta@gmail.com>
2019-03-20 16:35:29 +0700

    OpenCL sum working

--- sumcl.cl
+++ sumcl.cl
@@ -1,3 +1,12 @@
 kernel void simple_add(global const int* A, global const int* B, global int* C){
-	C[get_global_id(0)] = A[get_global_id(0)] + B[get_global_id(0)];
+	int workid = get_global_id(0);
+	int worksize = B[0] / get_global_size(0);
+	int startpos = workid * worksize;
+	int endpos = startpos + worksize;
+	int localsum = 0;
+	for(int i = startpos; i < endpos; i++){
+		localsum += A[i];
+	}
+	C[workid] = localsum;
+	printf("I got %d from work %d\n", localsum, workid);
 }
--- sumcl.cpp
+++ sumcl.cpp
@@ -3,71 +3,54 @@
 using namespace std;
 
 int main() {
-	//get all platforms (drivers)
-	std::vector<cl::Platform> all_platforms;
-	cl::Platform::get(&all_platforms);
-	if (all_platforms.size() == 0) {
-		std::cout << " No platforms found. Check OpenCL installation!\n";
-		exit(1);
-	}
-	cl::Platform default_platform = all_platforms[0];
-	std::cout << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
-
-	//get default device of the default platform
-	std::vector<cl::Device> all_devices;
-	default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
-	if (all_devices.size() == 0) {
-		std::cout << " No devices found. Check OpenCL installation!\n";
-		exit(1);
-	}
-	cl::Device default_device = all_devices[0];
-	std::cout << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
-
-	cl::Context context({ default_device });
-
-	cl::Program::Sources sources;
-
-	// kernel calculates for each element C=A+B
 	ifstream ifile;
-	ifile.open("sumcl.cl");
 	stringstream sstr;
-	sstr << ifile.rdbuf();
-	std::string kernel_code = sstr.str();
-	sources.push_back({ kernel_code.c_str(), kernel_code.length() });
+	string kernel_code;
+	vector<int> numdata;
+	int x;
 
-	cl::Program program(context, sources);
-	if (program.build({ default_device }) != CL_SUCCESS) {
-		std::cout << " Error building: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device) << "\n";
-		exit(1);
+	ifile.open("rand_data.txt");
+	while (ifile >> x) {
+		numdata.push_back(x);
 	}
+	ifile.close();
+	int numsize = numdata.size();
+
+	ifile.open("sumcl.cl");
+	sstr << ifile.rdbuf();
+	ifile.close();
+	kernel_code = sstr.str();
+
+	cl::Context context(CL_DEVICE_TYPE_DEFAULT);
+	cl::Program program(context, kernel_code, true);
+
+	vector<int> B(1);
+	B[0] = numsize;
+	vector<int> C(4);
 
 	// create buffers on the device
-	cl::Buffer buffer_A(context, CL_MEM_READ_WRITE, sizeof(int) * 10);
-	cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, sizeof(int) * 10);
-	cl::Buffer buffer_C(context, CL_MEM_READ_WRITE, sizeof(int) * 10);
-
-	int A[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int B[] = { 0, 1, 2, 0, 1, 2, 0, 1, 2, 0 };
+	cl::Buffer buffer_A(context, numdata.begin(), numdata.end(), true);
+	cl::Buffer buffer_B(context, B.begin(), B.end(), true);
+	cl::Buffer buffer_C(context, C.begin(), C.end(), false);
 
 	//create queue to which we will push commands for the device.
-	cl::CommandQueue queue(context, default_device);
-
-	//write arrays A and B to the device
-	queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, sizeof(int) * 10, A);
-	queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, sizeof(int) * 10, B);
+	cl::CommandQueue queue(context);
+	//write to buffer
+	queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, sizeof(int) * 100, &numdata[0]);
+	queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, sizeof(int), &B[0]);
+	cl::EnqueueArgs eargs(queue, cl::NDRange(4));
 
 	//run the kernel
-	auto simple_add = cl::make_kernel<cl::Buffer&, cl::Buffer&, cl::Buffer&>(program, "simple_add");
-	cl::EnqueueArgs eargs(queue, cl::NullRange, cl::NDRange(10), cl::NullRange);
+	cl::make_kernel simple_add = cl::make_kernel<cl::Buffer&, cl::Buffer&, cl::Buffer&>(program, "simple_add");
 	simple_add(eargs, buffer_A, buffer_B, buffer_C).wait();
 
-	int C[10];
-	//read result C from the device to array C
-	queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, sizeof(int) * 10, C);
+	queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, sizeof(int) * 4, &C[0]);
 
-	cout << " result: \n";
-	for (int i = 0; i < 10; i++) {
-		cout << C[i] << " ";
+	//calculate final sum
+	int result = 0;
+	for (int i = 0; i < 4; i++) {
+		result += C[i];
 	}
-	cout << endl;
+
+	printf("Total sum: %d\n", result);
 }
